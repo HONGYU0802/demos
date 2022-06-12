be1d7f6974fcefacf7265075c287571559c05b03
macbook <hongyu.0802@gmail.com>
2022-06-12 23:14:59 +0800

    custom

--- React/router-tutorial/src/Routes/Invoices.js
+++ React/router-tutorial/src/Routes/Invoices.js
@@ -1,9 +1,15 @@
-import { NavLink , Outlet, useSearchParams} from "react-router-dom";
+import { NavLink , Outlet, useSearchParams, useLocation} from "react-router-dom";
 import { getInvoices } from "../data";
+
+function QueryNavLink({ to, ...props }) {
+    let location = useLocation();
+    return <NavLink to={to + location.search} {...props} />;
+  }
 
 export default function Invoices() {
   let invoices = getInvoices();
   let [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams)
 
   return (
     <div style={{ display: "flex" }}>
@@ -32,7 +38,7 @@ export default function Invoices() {
             return name.startsWith(filter.toLowerCase());
           })
         .map((invoice) => (
-            <NavLink
+            <QueryNavLink
                 style={({ isActive }) => {
                     return {
                     display: "block",
@@ -44,7 +50,7 @@ export default function Invoices() {
                 key={invoice.number}
                 >
                 {invoice.name}
-            </NavLink>
+            </QueryNavLink>
         ))}
       </nav>
       <Outlet />
